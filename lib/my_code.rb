ba9eac091144de043c9d55402619b77a4c415137
Wesley Chen <weschen1996@gmail.com>
2020-01-19 01:06:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,11 +2,8 @@
 def map(arr, block)
   ans = []
   count = 0
-  while count < arr.length; 
-     arr[count]
-  
-    
-    ans.push
+  while count < arr.length;
+    ans.push(yield(arr[count]))
     count += 1
   end
   return ans
