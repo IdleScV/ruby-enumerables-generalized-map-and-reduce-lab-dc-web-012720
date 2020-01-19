b0eba7ebb8285b6868498564d89dc40142d4af7c
Wesley Chen <weschen1996@gmail.com>
2020-01-19 00:43:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
 # Your Code Here
+def map(arr, block)
+  ans = []
+  count = 0
+  while count < arr.length; 
+    ans.push(
+      
+      arr[count]
+      
+      )
+    count += 1
+  end
+  return ans
+end
