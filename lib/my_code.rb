f5a185cd2d129d2e8795d3ab43c4a81367426f53
Wesley Chen <weschen1996@gmail.com>
2020-01-19 01:26:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,13 +12,16 @@ end
 def reduce(arr, block=nil)
   ans=0
   count = 0
+  
   if block != nil
     ans = block
     count += 1
   end
+  
   while count < arr.length 
     ans = yield(ans, arr[count])
     count += 1
   end
+  
   return ans
 end
